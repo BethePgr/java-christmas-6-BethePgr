6106447f67a855211e52672d70a52bc8e552589c
jongwonpc <c3392302@naver.com>
2023-11-11 04:05:15 +0900

    feat: 입력한 날짜에 해당되는 이벤트 미리보기 구문 출력 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public static void printStartEventPlanner() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
+
+    public static void printShowEvent(int day) {
+        System.out.println("12월 " + day + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    }
 }
