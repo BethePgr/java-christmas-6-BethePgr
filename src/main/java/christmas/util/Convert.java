df969d1f74fb40325467f3c168ad3f57a5d5d719
jongwonpc <c3392302@naver.com>
2023-11-11 04:11:40 +0900

    feat: 입력한 메뉴 및 수량을 map으로 변환하는 기능 구현

---
+++ src/main/java/christmas/util/Convert.java
@@ -0,0 +1,25 @@
+package christmas.util;
+
+import static christmas.domain.Menu.findMenuByName;
+
+import christmas.domain.Menu;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Convert {
+
+    public static Map<Menu, Integer> convertToMenuMap(String input) {
+        Map<Menu, Integer> menuMap = new HashMap<>();
+        List<String> menus = Arrays.asList(input.split(","));
+        for (String menu : menus) {
+            List<String> menuOptions = Arrays.asList(menu.split("-"));
+            String menuName = menuOptions.get(0);
+            int menuQuantity = Integer.parseInt(menuOptions.get(1));
+            menuMap.put(findMenuByName(menuName), menuQuantity);
+        }
+        return menuMap;
+    }
+
+}
