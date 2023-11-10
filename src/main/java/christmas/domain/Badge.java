7f510ecabe8b690bc1c25273e5b9442339197f0b
jongwonpc <c3392302@naver.com>
2023-11-11 05:47:10 +0900

    feat: 뱃지 enum으로 구현

---
+++ src/main/java/christmas/domain/Badge.java
@@ -0,0 +1,31 @@
+package christmas.domain;
+
+import java.util.Arrays;
+
+public enum Badge {
+
+    SANTA("산타", 20000),
+    TREE("트리", 10000),
+    STAR("별", 5000);
+
+    private String name;
+    private int price;
+
+    Badge(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public static Badge findBadgeByPrice(int price) {
+        return Arrays.stream(Badge.values()).filter(badge -> badge.getPrice() <= price).findFirst().orElse(null);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+}
