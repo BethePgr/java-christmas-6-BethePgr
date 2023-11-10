a8678c6e44b9093d39cc6cdcb0401c0a5b2d2e04
jongwonpc <c3392302@naver.com>
2023-11-11 04:28:33 +0900

    feat: 샴페인 증정 이벤트 여부 확인하는 기능 구현

--- src/main/java/christmas/domain/ChristmasEvent.java
+++ src/main/java/christmas/domain/ChristmasEvent.java
@@ -1,11 +1,15 @@
 package christmas.domain;
 
+import static christmas.domain.Menu.CHAMPAGNE;
+
 import christmas.util.Convert;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ChristmasEvent {
 
     private final Map<Menu, Integer> menuMap;
+    private final Map<Event, Integer> eventMap = new HashMap<>();
 
     public ChristmasEvent(String input) {
         menuMap = Convert.convertToMenuMap(input);
@@ -17,5 +21,11 @@ public class ChristmasEvent {
             price += menu.getPrice() * menuMap.get(menu);
         }
         return price;
+    }
+
+    public void checkGiftEvent(int price) {
+        if (price >= 120000) {
+            eventMap.put(Event.GIFT_EVENT, CHAMPAGNE.getPrice());
+        }
     }
 }
