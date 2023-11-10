53376b14bef6c793e0834c31b2544523f587d3c9
jongwonpc <c3392302@naver.com>
2023-11-11 04:14:26 +0900

    feat: 모든 메뉴의 이름과 수량를 출력하는 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,8 @@
 package christmas.view;
 
+import christmas.domain.Menu;
+import java.util.Map;
+
 public class OutputView {
 
     public static void printStartEventPlanner() {
@@ -8,5 +11,12 @@ public class OutputView {
 
     public static void printShowEvent(int day) {
         System.out.println("12월 " + day + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    }
+
+    public static void printAllMenus(Map<Menu, Integer> menuMap) {
+        System.out.println("<주문 메뉴>");
+        for (Menu menu : menuMap.keySet()) {
+            System.out.println(menu.getName() + menuMap.get(menu) + "개");
+        }
     }
 }
