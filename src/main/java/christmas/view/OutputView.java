c163eeeee18b1a475d352f39804951c2ec825d9d
jongwonpc <c3392302@naver.com>
2023-11-12 00:12:19 +0900

    refactor: outputView 메세지 상수화

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,20 @@
 package christmas.view;
 
+import static christmas.constant.ViewMessage.AFTER_DISCOUNT_PRICE_MESSAGE;
+import static christmas.constant.ViewMessage.ALL_DISCOUNT_AMOUNT;
+import static christmas.constant.ViewMessage.ALL_DISCOUNT_MESSAGE;
+import static christmas.constant.ViewMessage.BEFORE_DISCOUNT_PRICE_MESSAGE;
+import static christmas.constant.ViewMessage.DECEMBER_BADGE;
+import static christmas.constant.ViewMessage.EVENT_DISCOUNT_AMOUNT;
+import static christmas.constant.ViewMessage.EVENT_LIST_MESSAGE;
+import static christmas.constant.ViewMessage.EVENT_PLANNER_MESSAGE;
+import static christmas.constant.ViewMessage.GIFT_MENU_MESSAGE;
+import static christmas.constant.ViewMessage.MENU_QUANTITY;
+import static christmas.constant.ViewMessage.NOTHING;
+import static christmas.constant.ViewMessage.ORDERED_MENU_MESSAGE;
+import static christmas.constant.ViewMessage.PRICE_WON;
+import static christmas.constant.ViewMessage.SHOW_EVENT_MESSAGE;
+import static christmas.constant.ViewMessage.ZERO_WON;
 import static christmas.util.Convert.convertAmount;
 
 import christmas.domain.Badge;
@@ -10,64 +25,64 @@ import java.util.Map;
 public class OutputView {
 
     public static void printStartEventPlanner() {
-        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println(EVENT_PLANNER_MESSAGE);
     }
 
     public static void printShowEvent(int day) {
-        System.out.println("12월 " + day + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        System.out.printf(SHOW_EVENT_MESSAGE, day);
     }
 
     public static void printAllMenus(Map<Menu, Integer> menuMap) {
-        System.out.println("<주문 메뉴>");
+        System.out.println(ORDERED_MENU_MESSAGE);
         for (Menu menu : menuMap.keySet()) {
-            System.out.println(menu.getName() + " " + menuMap.get(menu) + "개");
+            System.out.printf(MENU_QUANTITY, menu.getName(), menuMap.get(menu));
         }
     }
 
     public static void printBeforeEventPrice(int price) {
-        System.out.println("<할인 전 총주문 금액>");
-        System.out.println(convertAmount(price) + "원");
+        System.out.println(BEFORE_DISCOUNT_PRICE_MESSAGE);
+        System.out.printf(PRICE_WON, convertAmount(price));
     }
 
     public static void printGiftEvent(boolean giftEventExist) {
-        System.out.println("<증정 메뉴>");
+        System.out.println(GIFT_MENU_MESSAGE);
         if (giftEventExist) {
             System.out.println("샴페인 1개");
         }
-        System.out.println("없음");
+        System.out.println(NOTHING);
     }
 
     public static void printEvents(Map<Event, Integer> eventMap) {
-        System.out.println("<혜택 내역>");
+        System.out.println(EVENT_LIST_MESSAGE);
         if (eventMap.size() == 0) {
-            System.out.println("없음");
+            System.out.println(NOTHING);
         }
         if (eventMap.size() != 0) {
             for (Event event : eventMap.keySet()) {
-                System.out.println(event.getName() + ": -" + convertAmount(eventMap.get(event)) + "원");
+                System.out.printf(EVENT_DISCOUNT_AMOUNT, event.getName(), convertAmount(eventMap.get(event)));
             }
         }
     }
 
     public static void printDiscount(int price) {
-        System.out.println("<총혜택 금액>");
+        System.out.println(ALL_DISCOUNT_MESSAGE);
         if (price == 0) {
-            System.out.println("0원");
+            System.out.println(ZERO_WON);
         }
         if (price != 0) {
-            System.out.println("-" + convertAmount(price) + "원");
+            System.out.printf(ALL_DISCOUNT_AMOUNT, convertAmount(price));
         }
     }
 
     public static void printFinalPayAmount(int price) {
-        System.out.println("<할인 후 예상 결제 금액>");
-        System.out.println(convertAmount(price) + "원");
+        System.out.println(AFTER_DISCOUNT_PRICE_MESSAGE);
+        System.out.printf(PRICE_WON, convertAmount(price));
     }
 
     public static void printBadge(Badge badge) {
-        System.out.println("<12월 이벤트 배지>");
+        System.out.println(DECEMBER_BADGE);
         if (badge == null) {
-            System.out.println("없음");
+            System.out.println(NOTHING);
         }
         if (badge != null) {
             System.out.println(badge.getName());
