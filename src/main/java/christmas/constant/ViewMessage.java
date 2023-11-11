e1b22d0f5455eaeae3879c5226bf602175dd999b
jongwonpc <c3392302@naver.com>
2023-11-12 00:18:11 +0900

    style: 양식에 맞게 줄넘기기 적용

--- src/main/java/christmas/constant/ViewMessage.java
+++ src/main/java/christmas/constant/ViewMessage.java
@@ -3,21 +3,24 @@ package christmas.constant;
 public class ViewMessage {
 
     public static final String EVENT_PLANNER_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
-    public static final String SHOW_EVENT_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
-    public static final String ORDERED_MENU_MESSAGE = "<주문 메뉴>";
-    public static final String MENU_QUANTITY = "%s %d개";
-    public static final String BEFORE_DISCOUNT_PRICE_MESSAGE = "<할인 전 총주문 금액>";
-    public static final String PRICE_WON = "%s원";
-    public static final String GIFT_MENU_MESSAGE = "<증정 메뉴>";
+    public static final String SHOW_EVENT_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
 
-    public static final String EVENT_LIST_MESSAGE = "<혜택 내역>";
-    public static final String EVENT_DISCOUNT_AMOUNT = "%s: -%s원";
+    public static final String ORDERED_MENU_MESSAGE = "\n<주문 메뉴>";
+    public static final String MENU_QUANTITY = "%s %d개\n";
 
-    public static final String ALL_DISCOUNT_MESSAGE = "<총혜택 금액>";
-    public static final String ALL_DISCOUNT_AMOUNT = "-%s원";
+    public static final String BEFORE_DISCOUNT_PRICE_MESSAGE = "\n<할인 전 총주문 금액>";
+    public static final String PRICE_WON = "%s원\n";
 
-    public static final String AFTER_DISCOUNT_PRICE_MESSAGE = "<할인 후 예상 결제 금액>";
-    public static final String DECEMBER_BADGE = "<12월 이벤트 배지>";
+    public static final String GIFT_MENU_MESSAGE = "\n<증정 메뉴>";
+
+    public static final String EVENT_LIST_MESSAGE = "\n<혜택 내역>";
+    public static final String EVENT_DISCOUNT_AMOUNT = "%s: -%s원\n";
+
+    public static final String ALL_DISCOUNT_MESSAGE = "\n<총혜택 금액>";
+    public static final String ALL_DISCOUNT_AMOUNT = "-%s원\n";
+
+    public static final String AFTER_DISCOUNT_PRICE_MESSAGE = "\n<할인 후 예상 결제 금액>";
+    public static final String DECEMBER_BADGE = "\n<12월 이벤트 배지>";
 
     public static final String NOTHING = "없음";
     public static final String ZERO_WON = "0원";
