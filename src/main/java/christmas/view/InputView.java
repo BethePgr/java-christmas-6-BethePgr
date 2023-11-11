43b64d9e3c5bf167ad5c49fe687669f6ee2192f0
jongwonpc <c3392302@naver.com>
2023-11-12 00:19:59 +0900

    refactor: inputView 메세지 상수화

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,16 +1,19 @@
 package christmas.view;
 
+import static christmas.constant.ViewMessage.INPUT_MENU_NAME_QUANTITY;
+import static christmas.constant.ViewMessage.INPUT_VISIT_DAY_MESSAGE;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
     public static String inputDayOfDecember() {
-        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        System.out.println(INPUT_VISIT_DAY_MESSAGE);
         return Console.readLine();
     }
 
     public static String inputMenuNameAndQuantity() {
-        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        System.out.println(INPUT_MENU_NAME_QUANTITY);
         return Console.readLine();
     }
 }
